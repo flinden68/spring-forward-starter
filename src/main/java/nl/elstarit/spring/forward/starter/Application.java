f5b17c40ee5646846d41d862a398b02e9ec00a9f
Frank van der Linden <flinden68@elstarit.nl>
2018-05-21 22:15:50 +0200

    replaced the deprecated WebMvcConfigurerAdapter by WebMvcConfigurer

--- src/main/java/nl/elstarit/spring/forward/starter/Application.java
+++ src/main/java/nl/elstarit/spring/forward/starter/Application.java
@@ -7,14 +7,14 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.LocaleResolver;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
 import org.springframework.web.servlet.i18n.SessionLocaleResolver;
 
 import java.util.Locale;
 
 @SpringBootApplication
-public class Application extends WebMvcConfigurerAdapter {
+public class Application implements WebMvcConfigurer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
