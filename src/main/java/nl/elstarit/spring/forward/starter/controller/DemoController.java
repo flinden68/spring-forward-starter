e10d73743e7e98b16279d90077b5ff0ced8b5476
Frank van der Linden <flinden68@elstarit.nl>
2018-09-11 16:13:48 +0200

    Upgraded to Swagger 2.9.2

--- src/main/java/nl/elstarit/spring/forward/starter/controller/DemoController.java
+++ src/main/java/nl/elstarit/spring/forward/starter/controller/DemoController.java
@@ -1,12 +1,8 @@
 package nl.elstarit.spring.forward.starter.controller;
 
-import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
 import nl.elstarit.spring.forward.starter.model.Demo;
 import nl.elstarit.spring.forward.starter.service.DemoService;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -21,16 +17,11 @@ import static org.springframework.web.bind.annotation.RequestMethod.GET;
 @Slf4j
 @Controller
 @RequestMapping("/api")
-@Api(description = "Demo API", tags = {"Demo"})
 public class DemoController {
 
   @Autowired
   private DemoService demoService;
 
-  @ApiOperation(value = "Get a list demo objects", notes = "Additional notes for this endpoint")
-  @ApiResponses(value = {
-    @ApiResponse(code = 200, message = "Return list of demo objects"),
-    @ApiResponse(code = 500, message = "Internal error, go check the logs")})
   @RequestMapping(method = GET, value="/demos", produces = "application/json")
   public ResponseEntity<?> getDemos(@RequestParam(value = "amount") int amount){
     List<Demo> todos = demoService.generateListOfDemoObjects(amount);
